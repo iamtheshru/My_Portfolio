7c6d35dc2d155c4ba855ede073516474607232ef
iamtheshru <s.j.patel.3110@gmail.com>
2025-03-03 00:02:41 +0530

    data

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { CgGitFork } from "react-icons/cg";
-import profilelogo from "../../Assets/shraddhaPatel.jpeg";
+import profilelogo from "../Assets/shraddhaPatel.jpeg";
 import {
   AiFillStar,
   AiOutlineHome,
